003b50daf0c1bf8fd856a29f8c6ad3d37098c7e6
Siddhanth  Shah <siddhanth.shah@somaiya.edu>
2023-07-26 21:57:19 +0530

    footer_fixed

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -38,18 +38,18 @@ const Footer = () => {
     
       <div className="row mt-3">
         <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <h6 className="text-uppercase fw-bold mb-4">
+          <p className="text-uppercase fw-bold mb-4">
             <i className="fas fa-gem me-3" />Company name
-          </h6>
+          </p>
           <p>
             Here you can use rows and columns to organize your footer content. Lorem ipsum
             dolor sit amet, consectetur adipisicing elit.
           </p>
         </div>
         <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <h6 className="text-uppercase fw-bold mb-4">
+          <p className="text-uppercase fw-bold mb-4">
             Products
-          </h6>
+          </p>
           <p>
             <a href="#!" className="text-reset">Angular</a>
           </p>
@@ -64,9 +64,9 @@ const Footer = () => {
           </p>
         </div>
         <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-          <h6 className="text-uppercase fw-bold mb-4">
+          <p className="text-uppercase fw-bold mb-4">
             Useful links
-          </h6>
+          </p>
           <p>
             <a href="#!" className="text-reset">Pricing</a>
           </p>
@@ -81,7 +81,7 @@ const Footer = () => {
           </p>
         </div>
         <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+          <p className="text-uppercase fw-bold mb-4">Contact</p>
           <p><i className="fas fa-home me-3" /> New York, NY 10012, US</p>
           <p>
             <i className="fas fa-envelope me-3" />
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import './Home.css';
 import About from '../About/About';
 import Downloads from '../Download/Download';
@@ -37,7 +36,6 @@ function Home() {
         google={window.google}
       /> 
       </div>*/}
-      <BgColorExample />
       </div>
       <div id='download'><Downloads /></div>
       <div id='about'><About /></div>
@@ -52,62 +50,6 @@ export default Home
 
 
 
-function BgColorExample() {
-  const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const windowHeight = window.innerHeight;
-      const scrollY = window.scrollY || window.pageYOffset;
-      const bodyHeight = document.body.offsetHeight;
-      const scrollPercentage = (scrollY / (bodyHeight - windowHeight)) * 100;
 
-      if (scrollPercentage > 50) {
-        setIsVisible(true);
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  return (
-    <div className="card-container justify-content-evenly mt-4 pt-4">
-      <div className={`left-cards ${isVisible ? 'visible' : ''}`}>
-        {['Primary', 'Secondary'].map((variant, index) => (
-          <Card
-            bg={variant.toLowerCase()}
-            key={variant}
-            text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-            style={{ width: '24rem', margin: '2rem' }}
-            className="mb-2"
-          >
-            <Card.Header>Header</Card.Header>
-            <Card.Body>
-              <Card.Title>{variant} Card Title</Card.Title>
-              <Card.Text>test</Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
-      <div className={`right-cards ${isVisible ? 'visible' : ''}`}>
-        {['Success', 'Danger'].map((variant, index) => (
-          <Card
-            bg={variant.toLowerCase()}
-            key={variant}
-            text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-            style={{ width: '24rem', margin: '2rem' }}
-            className="mb-2"
-          >
-            <Card.Header>Header</Card.Header>
-            <Card.Body>
-              <Card.Title>{variant} Card Title</Card.Title>
-              <Card.Text>test</Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
-    </div>
-  );
-}
+  
