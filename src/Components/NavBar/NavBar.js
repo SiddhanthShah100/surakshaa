e65dfc6673c745db9dee01bc936228d3770226c0
Siddhanth Shah <siddhanth.shah@somaiya.edu>
2023-07-29 01:29:30 +0530

    Revert "added features"
    This reverts commit fba8c812116c84f7c474118e16ed514b44fe247c.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,28 +1,3 @@
-// import { HashLink } from 'react-router-hash-link';
-// import './NavBarr.css';
-// import { useState } from 'react';
-
-// const NavBar =() => {
-// const [open, setOpen] = useState(false)
-//   return (
-//     <div>
-//         <nav className='sticky'>
-//             <div className="logo">Suraksha</div>
-//             <ul className="nav-links" onClick={() => setOpen(!open)} style={{transform: open ? "translateX(0px)" : ""}}>
-//             <HashLink  to="#home" eventKey={0}>Home</HashLink>
-//             <HashLink  to="#download" eventKey={1}>Download</HashLink>
-//             <HashLink  to="#about" eventKey={2}>Contact Us</HashLink>
-//             </ul>
-//             <i onClick={() => setOpen(!open)} className="fas fa-bars Menu"></i>
-//         </nav>
-//     </div>
-//   );
-// }
-
-// export default NavBar;
-
-
-
 import { HashLink } from 'react-router-hash-link';
 import './NavBarr.css';
 import { useState } from 'react';
@@ -32,11 +7,11 @@ const [open, setOpen] = useState(false)
   return (
     <div>
         <nav className='sticky'>
-            <div className="logo">Surakshaa</div>
+            <div className="logo">Suraksha</div>
             <ul className="nav-links" onClick={() => setOpen(!open)} style={{transform: open ? "translateX(0px)" : ""}}>
             <HashLink  to="#home" eventKey={0}>Home</HashLink>
-            <HashLink  to="#about" eventKey={3}>Contact Us</HashLink>
-            <HashLink className='download-btn' to="https://www.google.com" >Download</HashLink>
+            <HashLink  to="#download" eventKey={1}>Download</HashLink>
+            <HashLink  to="#about" eventKey={2}>Contact Us</HashLink>
             </ul>
             <i onClick={() => setOpen(!open)} className="fas fa-bars Menu"></i>
         </nav>
